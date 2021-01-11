186daf2e9f7cc6d0baa8584c9fba88b5c29bc779
Forest <azureforestryan@gmail.com>
2021-01-11 08:21:06 +0000

    update state and array immutably

--- README.md
+++ README.md
@@ -3,3 +3,15 @@ Redux is an independent, third-party library. It is for state management.
 
 ## Redux Flow
 Component (wants to manipulate app state) -> dispatches Actions -> Action reaches Reducers -> Reducers receive action and update State in Central Store -> Central Store triggers Subscription -> Subscription passes updated state to application.
+
+## tips
+- Updating Nested Objects
+    - The key to updating the nested data is that every level of nesting must be copied and updated appropriately. 
+    - You are encouraged to keep your state flattened and compose reducers as much as possible, because copying nested data can make code more difficult to read.
+    - Inserting and removing items in Arrays
+        - Copy first and mutable later
+        - Only copy the parts needed to implement deletion
+        - Using filter to implement deletion
+        - As long as you don't mutate the original array
+    -Updating an Item in an Array
+        - Use Ay.map to return a new value for a item we want to update
--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -1,18 +1,26 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {connect} from 'react-redux';
 
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
 
 class Counter extends Component {
-    render () {
+    render() {
         return (
             <div>
-                <CounterOutput value={this.props.ctr} />
-                <CounterControl label="Increment" clicked={this.props.onIncrementCounter} />
-                <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}  />
-                <CounterControl label="Add 5" clicked={() => this.props.onAddCounter(5)}  />
-                <CounterControl label="Subtract 5" clicked={() => this.props.onSubtractCounter(5)}  />
+                <CounterOutput value={this.props.ctr}/>
+                <CounterControl label="Increment" clicked={this.props.onIncrementCounter}/>
+                <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}/>
+                <CounterControl label="Add 5" clicked={() => this.props.onAddCounter(5)}/>
+                <CounterControl label="Subtract 5" clicked={() => this.props.onSubtractCounter(5)}/>
+                <hr/>
+                <button onClick={this.props.onStoreResult}>Store Result</button>
+                <ul>
+                    {this.props.storedResults.map(strResult => (
+                        <li onClick={() => this.props.onDeleteResult(strResult.id)}
+                            key={strResult.id}>{strResult.value}</li>
+                    ))}
+                </ul>
             </div>
         );
     }
@@ -20,7 +28,8 @@ class Counter extends Component {
 
 const mapStateToProps = state => {
     return {
-        ctr: state.counter
+        ctr: state.counter,
+        storedResults: state.results
     };
 };
 
@@ -29,7 +38,9 @@ const mapDispatchToProps = dispatch => {
         onIncrementCounter: () => dispatch({type: 'INCREMENT'}),
         onDecrementCounter: () => dispatch({type: 'DECREMENT'}),
         onAddCounter: (val) => dispatch({type: 'ADD', value: val}),
-        onSubtractCounter: (val) => dispatch({type: 'SUBTRACT', value: val})
+        onSubtractCounter: (val) => dispatch({type: 'SUBTRACT', value: val}),
+        onStoreResult: () => dispatch({type: 'STORE_RESULT'}),
+        onDeleteResult: (id) => dispatch({type: 'DELETE_RESULT', resultElId: id})
     };
 };
 
